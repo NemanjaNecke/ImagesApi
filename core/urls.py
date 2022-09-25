a915ae80a2ce307f31299e5ad76b8b7f8b1aeb0e
Nemanja <n.filkic@gmail.com>
2022-09-26 01:30:49 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('images/', include('images.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns +=static(settings.STATIC_URL, document.root=Settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document.root=Settings.STATIC_ROOT)
