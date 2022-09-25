9c09f2ffae5ea3b008359d1e4a7e313edf2ce50d
Nemanja <n.filkic@gmail.com>
2022-09-26 01:47:41 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/home/natasamodrakovic/imagesAPI/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/home/natasamodrakovic/imagesAPI/staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/home/natasamodrakovic/imagesAPi/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
